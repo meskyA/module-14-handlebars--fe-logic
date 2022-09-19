1243accb8669ec8b4cd08cfba77c2b36d2c5f7c6
meskyA <mesky2015@gmail.com>
2022-09-18 20:45:54 -0700

    initial commit

---
+++ Unsolved/public/js/edit-dish.js
@@ -0,0 +1,44 @@
+async function editFormHandler(event) {
+  event.preventDefault();
+  const dish_name = document.querySelector('#dish_name').value;
+  const description = document.querySelector('#description').value;
+  const guest_name = document.querySelector('#guest_name').value;
+  
+  // TODO: What will the value of has_nuts be if the box in the form is checked? 
+  // has nuts
+  // TODO: What do we call this kind of operator?
+  // quety selector
+  const has_nuts = document.querySelector('#has_nuts:checked') ? true : false;
+
+// window.location gives us access to the URL. We then use the .split() method to access the number at the end of the URL and set that equal to id.
+  const id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
+
+  // TODO: What part of our application will handle this 'put' request?
+  
+
+
+  const response = await fetch(`/api/dish/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      dish_name,
+      description,
+      guest_name,
+      has_nuts,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  // TODO: What happens if the response is ok?
+  // dish gets edited
+  if (response.ok) {
+    document.location.replace(`/dish/${id}`);
+  } else {
+    alert('Failed to edit dish');
+  }
+}
+
+document.querySelector('.edit-dish-form').addEventListener('submit', editFormHandler);
